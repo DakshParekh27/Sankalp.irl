2adee4cc501a8eadb6e6e33eee3c81f0d256bc98
Swastik Gupta <swastik4157stella@gmail.com>
2026-03-13 00:23:44 +0530

    build(frontend): install axios, setup proxy, and update vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,4 +8,18 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/auth': {
+        target: 'http://backend:5000',
+        changeOrigin: true
+      },
+      '/public': {
+        target: 'http://backend:5000',
+        changeOrigin: true
+      }
+    }
+  }
 })
